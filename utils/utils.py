bc2c18581ea76a6861d75952247868ea7809d20b
SamSamhuns <samhunsadamant@gmail.com>
2021-06-02 23:26:52 +0400

    Rewrite argparse help and del old color func

--- utils/utils.py
+++ utils/utils.py
@@ -15,24 +15,24 @@ def parse_arguments(desc):
                         # default='models/person-detection-0200/person-detection-0200.xml',                # 37 fps
                         default='models/person-detection-0201/person-detection-0201.xml',                # 30 fps
                         # default='models/person-detection-0202/person-detection-0202.xml',                # 23 FPS
-                        help='XML File')
+                        help='OpenVINO XML File')
     parser.add_argument('--pdet-model-bin',
                         # default='models/person-detection-retail-0013/person-detection-retail-0013.bin',  # 31 fps
                         # default='models/person-detection-0200/person-detection-0200.bin',                # 37 fps
                         default='models/person-detection-0201/person-detection-0201.bin',                # 30 fps
                         # default='models/person-detection-0202/person-detection-0202.bin',                # 23 FPS
-                        help='BIN File')
+                        help='OpenVINO BIN File')
     # person re-identification models
     parser.add_argument('--preid-model-xml',
                         # default="models/person-reidentification-retail-0277-fp32/person-reidentification-retail-0277.xml",  # slowest
                         default="models/person-reidentification-retail-0286-fp32/person-reidentification-retail-0286.xml",  # balanced
                         # default="models/person-reidentification-retail-0288-fp32/person-reidentification-retail-0288.xml",  # fastest
-                        help='XML File')
+                        help='OpenVINO XML File')
     parser.add_argument('--preid-model-bin',
                         # default="models/person-reidentification-retail-0277-fp32/person-reidentification-retail-0277.bin",  # slowest
                         default="models/person-reidentification-retail-0286-fp32/person-reidentification-retail-0286.bin",  # balanced
                         # default="models/person-reidentification-retail-0288-fp32/person-reidentification-retail-0288.bin",  # fastest
-                        help='BIN File')
+                        help='OpenVINO BIN File')
     # general arguments
     parser.add_argument('-d', '--target-device',
                         default='CPU', type=str,
@@ -43,7 +43,7 @@ def parse_arguments(desc):
                         help='Type of Input: image, video, cam')
     parser.add_argument('-i', '--input',
                         default='media/img/people_walking.jpg',  type=str,
-                        help='Path to Input: WebCam: 0, Video File or Image file')
+                        help='Path to input media file')
     parser.add_argument('-o', '--output_dir',
                         default='media/output',  type=str,
                         help='Output directory')
@@ -79,8 +79,6 @@ def get_openvino_core_net_exec(model_xml_path, model_bin_path, target_device="CP
 
 
 def get_distinct_rgb_color(index):
-    def hr(h):
-        return (int(h[0:2], 16), int(h[2:4], 16), int(h[4:6], 16))
     color_list = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255),
                   (0, 0, 0), (128, 0, 0), (0, 128, 0), (0, 0, 128), (128, 128, 0), (128, 0, 128),
                   (0, 128, 128), (128, 128, 128), (192, 0, 0), (0, 192, 0), (0, 0, 192), (192, 192, 0),
